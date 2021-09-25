00ba9dca24c7b5aaf5f0dfaf347e7dfab3f41854
lexingsen <lexingsen@163.com>
2021-09-25 09:20:14 +0800

    add 342

---
+++ 数论&数学/342-4的幂.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n) {
+            if (n == 1) {
+                return true;
+            }
+            if (n % 4 == 0) {
+                n /= 4;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
