bea7b9de4005aec07fae619b0fa24b16fe5cde8f
lexingsen <lexingsen@163.com>
2021-09-28 10:18:23 +0800

    add 557

---
+++ 字符串/557-反转字符串中的单词 III.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') {
+                continue;
+            }
+            int j = i;
+            while (j < s.size() && s[j] != ' ') {
+                j++;
+            }
+            reverse(s.begin() + i, s.begin() + j);
+            i = j - 1;
+        }
+        return s;
+    }
+};
