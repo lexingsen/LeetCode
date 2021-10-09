3851a8478b4f473d18d21da697913bb127783548
lexingsen <lexingsen@163.com>
2021-10-09 08:07:21 +0800

    add 120

---
+++ 动态规划/120-三角形最小路径和.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+// 倒着递推
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& f) {
+        for (int i = f.size() - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; j++) {
+                f[i][j] += min(f[i + 1][j], f[i + 1][j + 1]);
+            }
+        }
+
+        return f[0][0];
+    }
+};
+
+/*
+2
+3 4
+6 5 7
+4 1 8 3
+*/
