4de923ef41c8373831111d2ba6df0ab9affc6aa9
lexingsen <lexingsen@163.com>
2021-10-15 00:46:07 +0800

    add 350

---
+++ 二分&双指针/350-两个数组的交集 II.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0;
+        int j = 0;
+        int n = nums1.size();
+        int m = nums2.size();
+        while (i < n && j < m) {
+            if (nums1[i] == nums2[j]) {
+                ans.emplace_back(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
