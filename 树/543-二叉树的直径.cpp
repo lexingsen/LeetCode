fe824bb82993ce0cc7d7c2e52ba0ff2c77f13aac
lexingsen <lexingsen@163.com>
2021-10-22 16:27:07 +0800

    add 543

---
+++ 树/543-二叉树的直径.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    int ans;
+    int diameterOfBinaryTree(TreeNode* root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+
+    int dfs(TreeNode* root) {
+        if (!root) {
+            return -1;
+        }
+        auto l = dfs(root->left) + 1;
+        auto r = dfs(root->right) + 1;
+        ans = max(ans, r + l);
+        return max(l, r);
+    }
+};
