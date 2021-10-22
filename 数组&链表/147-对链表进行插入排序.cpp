5acf579a7b23654dfd451a6ff76306b8e8339ca3
lexingsen <lexingsen@163.com>
2021-10-22 14:58:18 +0800

    add 147

---
+++ 数组&链表/147-对链表进行插入排序.cpp
@@ -0,0 +1,30 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* dummy = new ListNode(-1, head);
+        for (auto p = head; p;) {
+            auto cur = dummy;
+            auto next = p->next;
+            while (cur->next && cur->next->val <= p->val) {
+                cur = cur->next;
+            }
+
+            p->next = cur->next;
+            cur->next = p;
+            p = next;
+        }
+
+        return dummy->next;
+    }
+};
