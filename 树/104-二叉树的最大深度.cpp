80dcc1212fe0bd65a824b85b68b1c2a69d1e1edb
lexingsen <lexingsen@163.com>
2021-10-23 00:19:57 +0800

    add 104

---
+++ 树/104-二叉树的最大深度.cpp
@@ -0,0 +1,22 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+        return max(left, right) + 1;
+    }
+    int maxDepth(TreeNode* root) {
+        return dfs(root);
+    }
+};
